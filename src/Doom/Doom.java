640767149c87238150e9689202979bf2e58645f8
reisinger69 <basti4772@gmail.com>
2023-01-16 17:11:04 +0100

    es föd nur nu enemys zahlen auf 0 setzen. + Testen

--- src/Doom/Doom.java
+++ src/Doom/Doom.java
@@ -2,12 +2,15 @@ package Doom;
 
 import Doom.Recources.Move;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Doom {
 
     int positionOfPlayer = playfield.indexOf('@');
     final int WIDTH = 41;
+
+    int playerLives = 3;
 
     int enemy1 = 182;
     int enemy2 = 283;
@@ -38,9 +41,64 @@ public class Doom {
     public void play() {
         while (true) {
             System.out.println(playfield);
-            movePlayer(getInput());
+            if(movePlayer(getInput())){
+                break;
+            }
+            if (!checkForFight())  {
+                playerLives--;
+                if (playerLives == 0) {
+                    System.out.println("You Loose!");
+                    System.exit(0);
+                }
+            }
+            checkForFight();
             moveAllEnemys();
         }
+
+        System.out.println("You win!");
+    }
+
+    private boolean checkForFight() { //retursn true when player wins a fight or of there is no enemy
+        if (playfield.charAt(positionOfPlayer-1) == 'X') {
+            return fight(positionOfPlayer-1);
+        } else if(playfield.charAt(positionOfPlayer+1) == 'X') {
+            return fight(positionOfPlayer+1);
+        } else if(playfield.charAt(positionOfPlayer+41) == 'X') {
+            return fight(positionOfPlayer+41);
+        } else if(playfield.charAt(positionOfPlayer-41) == 'X') {
+            return fight(positionOfPlayer-41);
+        } else {
+            return true;
+        }
+    }
+
+    private boolean fight(int positionOfEnemy) {
+        StringBuilder sb = new StringBuilder(playfield);
+        Random rand = new Random();
+        int randomNum = rand.nextInt(5) + 1;
+        Scanner scanner = new Scanner(System.in);
+        int guess;
+        int tries = 0;
+        while (tries < 3) {
+            System.out.println("Guess a number between 1 and 5:");
+            if(scanner.hasNextInt()){
+                guess = scanner.nextInt();
+                if(guess > 0 && guess < 6){
+                    if (guess == randomNum) {
+                        sb.setCharAt(positionOfEnemy, ' ');
+                        playfield = sb.toString();
+                        return true;
+                    }
+                    tries++;
+                }else{
+                    System.out.println("Invalid input, please enter a number between 1 and 5");
+                }
+            }else{
+                System.out.println("Invalid input, please enter a number between 1 and 5");
+                scanner.next();
+            }
+        }
+        return false;
     }
 
     private void moveAllEnemys() {
@@ -59,7 +117,9 @@ public class Doom {
         int move = (int)(Math.random() * 4) + 1;
         switch (move) {
             case 1 -> {
-                if (playfield.charAt(enemy-1) == '#') {
+                if (enemy == 0) {
+                    return 0;
+                } else if (playfield.charAt(enemy-1) == '#'  || playfield.charAt(enemy-1) == '@' || playfield.charAt(enemy-1) == 'S') {
                     return moveEnemy(enemy);
                 } else {
                     field.setCharAt(enemy, ' ');
@@ -69,7 +129,9 @@ public class Doom {
                 }
             }
             case 2 -> {
-                if (playfield.charAt(enemy+1) == '#') {
+                if (enemy == 0) {
+                    return 0;
+                } else if (playfield.charAt(enemy+1) == '#' || playfield.charAt(enemy+1) == '@' || playfield.charAt(enemy+1) == 'S') {
                     return moveEnemy(enemy);
                 } else {
                     field.setCharAt(enemy, ' ');
@@ -79,7 +141,9 @@ public class Doom {
                 }
             }
             case 3 -> {
-                if (playfield.charAt(enemy-41) == '#') {
+                if (enemy == 0) {
+                    return 0;
+                } else if (playfield.charAt(enemy-41) == '#'  || playfield.charAt(enemy-41) == '@' || playfield.charAt(enemy-41) == 'S') {
                     return moveEnemy(enemy);
                 } else {
                     field.setCharAt(enemy, ' ');
@@ -89,7 +153,9 @@ public class Doom {
                 }
             }
             case 4 -> {
-                if (playfield.charAt(enemy+41) == '#') {
+                if (enemy == 0) {
+                    return 0;
+                } else if (playfield.charAt(enemy+41) == '#'  || playfield.charAt(enemy+41) == '@' || playfield.charAt(enemy+41) == 'S') {
                     return moveEnemy(enemy);
                 } else {
                     field.setCharAt(enemy, ' ');
@@ -102,54 +168,60 @@ public class Doom {
         return enemy;
     }
 
-    private void movePlayer(Move move) {
+    private void updatePosition(int positionToMove) {
+        StringBuilder sb = new StringBuilder(playfield);
+        sb.setCharAt(positionOfPlayer, ' ');
+        sb.setCharAt(positionToMove, '@');
+        positionOfPlayer = positionOfPlayer-(positionOfPlayer-positionToMove);
+        playfield = sb.toString();
+    }
+
+    private boolean movePlayer(Move move) {
         switch (move) {
             case LEFT -> {
-                if (playfield.charAt(positionOfPlayer-1) == '#' || playfield.charAt(positionOfPlayer-1) == '@' || playfield.charAt(positionOfPlayer-1) == 'S') {
+                if (playfield.charAt(positionOfPlayer-1) == '#') {
                     System.out.println("Hier geht es nicht weiter"); // Wand gefunden
-                    return;
+                    return false;
+                } else if(playfield.charAt(positionOfPlayer-1) == 'S') {
+                    updatePosition(positionOfPlayer-1);
+                    return true;
                 }
-                StringBuilder sb = new StringBuilder(playfield);
-                sb.setCharAt(positionOfPlayer, ' ');
-                sb.setCharAt(positionOfPlayer-1, '@');
-                positionOfPlayer--;
-                playfield = sb.toString();
+                updatePosition(positionOfPlayer-1);
+
             }
             case RIGHT -> {
-                if (playfield.charAt(positionOfPlayer+1) == '#' || playfield.charAt(positionOfPlayer-1) == '@' || playfield.charAt(positionOfPlayer-1) == 'S') {
+                if (playfield.charAt(positionOfPlayer+1) == '#') {
                     System.out.println("Hier geht es nicht weiter"); // Wand gefunden
-                    return;
+                    return false;
+                }else if(playfield.charAt(positionOfPlayer+1) == 'S') {
+                    updatePosition(positionOfPlayer+1);
+                    return true;
                 }
-                StringBuilder sb = new StringBuilder(playfield);
-                sb.setCharAt(positionOfPlayer, ' ');
-                sb.setCharAt(positionOfPlayer+1, '@');
-                positionOfPlayer++;
-                playfield = sb.toString();
+                updatePosition(positionOfPlayer+1);
             }
             case FORWARD -> {
-                if (playfield.charAt(positionOfPlayer-WIDTH) == '#' || playfield.charAt(positionOfPlayer-1) == '@' || playfield.charAt(positionOfPlayer-1) == 'S') {
+                if (playfield.charAt(positionOfPlayer-WIDTH) == '#') {
                     System.out.println("Hier geht es nicht weiter"); // Wand gefunden
-                    return;
+                    return false;
+                }else if(playfield.charAt(positionOfPlayer-WIDTH) == 'S') {
+                    updatePosition(positionOfPlayer-WIDTH);
+                    return true;
                 }
-                StringBuilder sb = new StringBuilder(playfield);
-                sb.setCharAt(positionOfPlayer, ' ');
-                sb.setCharAt(positionOfPlayer-WIDTH, '@');
-                positionOfPlayer-=WIDTH;
-                playfield = sb.toString();
+                updatePosition(positionOfPlayer-WIDTH);
             }
             case BACKWARD -> {
-                if (playfield.charAt(positionOfPlayer+WIDTH) == '#' || playfield.charAt(positionOfPlayer-1) == '@' || playfield.charAt(positionOfPlayer-1) == 'S') {
+                if (playfield.charAt(positionOfPlayer+WIDTH) == '#') {
                     System.out.println("Hier geht es nicht weiter"); // Wand gefunden
-                    return;
+                    return false;
+                }else if(playfield.charAt(positionOfPlayer+WIDTH) == 'S') {
+                    updatePosition(positionOfPlayer+WIDTH);
+                    return true;
                 }
-                StringBuilder sb = new StringBuilder(playfield);
-                sb.setCharAt(positionOfPlayer, ' ');
-                sb.setCharAt(positionOfPlayer+WIDTH, '@');
-                positionOfPlayer+=WIDTH;
-                playfield = sb.toString();
+                updatePosition(positionOfPlayer+WIDTH);
             }
 
         }
+        return false;
     }
 
     private Move getInput() {
